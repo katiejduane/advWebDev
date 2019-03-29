a03f98a6a5bd7bd661920f27b64083c34094018f
Katie Duane <katiejduane@gmail.com>
2019-03-28 22:10:22 -0400

    the stack and the heap

--- 6_asyncFoundations/theStackAndTheHeap.js
+++ 6_asyncFoundations/theStackAndTheHeap.js
@@ -1,3 +1,14 @@
+// The Stack: An ordered set of stak frames (the 'stack frame' contains the function that was 
+// invoked, the parameters that were passed into the function, and the current line number).
+// Most recently invoed function is on the top of the stack, the bottom of the stack is the 
+// first function invoked. The stack is processed from top to bottom.
+
+// The Heap: An area in memory where you data is stored. An example:
+// The object is created in the heap. obj (below) is only a REFERENCE to the object. 
+let obj = {firstName: "Unicorn", lastName: "Smith"}
+// New data is not created, only a copy of the reference:
+let referenceCopy = obj;
+
 function multiply(x, y) {
     return x * y;
 }
