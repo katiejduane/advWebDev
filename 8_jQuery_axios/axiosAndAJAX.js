5f53ab7f2da95a60219f2e3984e7293d7dae33d1
Katie Duane <katiejduane@gmail.com>
2019-04-03 14:12:23 -0400

    ron swanson exercise

--- 8_jQuery_axios/axiosAndAJAX.js
+++ 8_jQuery_axios/axiosAndAJAX.js
@@ -22,31 +22,31 @@ axios.get(url)
 })
 
 // axios error handling!
-var btn = document.querySelector("button");
-var section = document.querySelector("#comments");
-btn.addEventListener("click", sendRequest);
+// var btn = document.querySelector("button");
+// var section = document.querySelector("#comments");
+// btn.addEventListener("click", sendRequest);
 
-function sendRequest() {
-    axios.get("https://jsonplaaskjldceholder.typicode.com/comments", {
-        params: {
-            postId: 1
-        }
-    })
-        .then(addComments)
-        .catch(handleErrors)
-}
+// function sendRequest() {
+//     axios.get("https://jsonplaaskjldceholder.typicode.com/comments", {
+//         params: {
+//             postId: 1
+//         }
+//     })
+//         .then(addComments)
+//         .catch(handleErrors)
+// }
 
-function addComments(res) {
-    res.data.forEach(function (comment) {
-        appendComment(comment);
-    });
-}
+// function addComments(res) {
+//     res.data.forEach(function (comment) {
+//         appendComment(comment);
+//     });
+// }
 
-function appendComment(comment) {
-    var newP = document.createElement("p");
-    newP.innerText = comment.email;
-    section.appendChild(newP);
-}
+// function appendComment(comment) {
+//     var newP = document.createElement("p");
+//     newP.innerText = comment.email;
+//     section.appendChild(newP);
+// }
 
 // axios has a special property for the responses and the request; 'err'
 // it checks if there is an error with the response, or the request
@@ -61,7 +61,83 @@ function handleErrors(err) {
 }
 
 // RON SWANSON EXERCISE!
-var ronUrl = 'https://ron-swanson-quotes.herokuapp.com/v2/quotes';
+let ronUrl = 'https://ron-swanson-quotes.herokuapp.com/v2/quotes';
+let xhrButton = document.querySelector('#xhr');
+let fetchButton = document.querySelector('#fetch');
+let jQueryButton = document.querySelector('#jquery');
+let axiosButton = document.querySelector('#axios');
+let quoteBox = document.querySelector('#quote');
+
+//xhr
+xhrButton.addEventListener("click", function(){
+    let xhr = new XMLHttpRequest;
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState == 4 && xhr.status == 200){
+            let response = JSON.parse(xhr.responseText);
+            quoteBox.textContent = response[0]
+        }
+    }
+    xhr.open("GET", ronUrl);
+    xhr.send();
+})
+
+//fetch
+fetchButton.addEventListener('click', function(){
+    fetch(ronUrl)
+        .then(errorHandler)
+        .then(parseJSON)
+        .then(changeDOM)
+        .catch(printError)
+})
+
+
+function errorHandler(response){
+    if(!response.ok){
+        throw Error(response.status)
+    }
+    return response
+}
+
+function parseJSON(response){
+    return response.json().then(function(data){
+        console.log(data)
+        return data[0]
+    })
+}
+
+function changeDOM(data){
+    quoteBox.textContent = data
+}
+
+function printError(error){
+    console.log(error)
+}
+
+//jQuery
+$('#jquery').click(function () {
+    $.getJSON(ronUrl)
+        .done(function(data){
+            $('#quote').text(data[0])
+        })
+        .fail(function(){
+            alert("ron didn't answer!")
+        })
+})
+
+//axios
+axiosButton.addEventListener('click', function(){
+    axios.get(ronUrl)
+        .then(function(res){
+            let ronsAnswer = res.data[0]
+            quoteBox.textContent = ronsAnswer
+        })
+        .catch(function(){
+            console.log('oops. there was an error')
+        })
+})
+
+
+
 
 
 
